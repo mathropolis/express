0411b0b4c0e1e75d04428e1d6108009ff49b8fe8
Nasir <nasir5074@gmail.com>
2022-10-30 12:15:44 +0400

    changes

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   HOST: process.env.RDS_HOST || "localhost",
   USER: process.env.RDS_USER || "root",
-  PASSWORD: process.env.RDS_DB || "root",
-  DB: process.env.RDS_PASSWORD || "germany"
+  PASSWORD: process.env.RDS_PASSWORD || "root",
+  DB: process.env.RDS_DB || "germany"
 };
