f3c37d2a2a9fb7152a52c2122e93cd4b819c29ac
Nasir <nasir5074@gmail.com>
2022-10-30 12:48:19 +0400

    changes

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,12 @@
+// module.exports = {
+//   HOST: process.env.RDS_HOST || "localhost",
+//   USER: process.env.RDS_USER || "root",
+//   PASSWORD: process.env.RDS_PASSWORD || "root",
+//   DB: process.env.RDS_DB || "germany"
+// };
 module.exports = {
-  HOST: process.env.RDS_HOST || "localhost",
-  USER: process.env.RDS_USER || "root",
-  PASSWORD: process.env.RDS_PASSWORD || "root",
-  DB: process.env.RDS_DB || "germany"
+  HOST: process.env.RDS_HOST || "epapa-db.crf61yx0mz5e.eu-central-1.rds.amazonaws.com",
+  USER: process.env.RDS_USER || "admin",
+  PASSWORD: process.env.RDS_PASSWORD || "germany_root5074",
+  DB: process.env.RDS_DB || "epapa-db"
 };
