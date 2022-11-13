0d329367a7bc87bb09793218b2c0f6a8e374a5f5
Nasir <nasir5074@gmail.com>
2022-11-13 20:00:42 +0400

    changes

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -5,8 +5,8 @@
 //   DB: process.env.RDS_DB || "germany"
 // };
 module.exports = {
-  HOST: process.env.RDS_HOST || "localhost",
-  USER: process.env.RDS_USER || "root",
-  PASSWORD: process.env.RDS_PASSWORD || "root",
-  DB: process.env.RDS_DB || "germany"
+  HOST: process.env.DB_HOST || "localhost",
+  USER: process.env.DB_USER || "root",
+  PASSWORD: process.env.DB_PASSWORD || "root",
+  DB: process.env.DB_NAME || "germany"
 };
